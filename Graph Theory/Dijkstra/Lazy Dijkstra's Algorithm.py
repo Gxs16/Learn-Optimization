ff8135b5408ebf0b49c9c5a0e2a30705adf4673a
X.Guo <847486005@qq.com>
2021-01-29 17:49:59 +0800

    path

--- Graph Theory/Dijkstra/Lazy Dijkstra's Algorithm.py
+++ Graph Theory/Dijkstra/Lazy Dijkstra's Algorithm.py
@@ -31,10 +31,13 @@ def Dijkstra(Graph, start, end):
     node_queue.push((start, 0)) # 构造接下来要遍历的优先级队列
 
     while not node_queue.empty():
-        current_node = node_queue.pop()[0] # 队列中弹出下一个遍历的点
+        current_node, _min_distance = node_queue.pop() # 队列中弹出下一个遍历的点
+        # 当我们已经找到一个到达当前节点的更优路径时，就不需要再在这个点搜索了
+        if _min_distance > Graph.nodes[current_node]['min_dis']:
+            continue
+
         for successor in Graph.neighbors(current_node):
             arc = (current_node, successor)
-
             _distance = Graph.nodes[current_node]['min_dis'] \
                         + Graph.edges[arc]['length'] # 计算起始点通过当前点到达后继节点的距离
 
@@ -42,10 +45,16 @@ def Dijkstra(Graph, start, end):
             if _distance < Graph.nodes[successor]['min_dis']:
                 Graph.nodes[successor]['min_dis'] = _distance
                 node_queue.push((successor, _distance)) # 在队列中加入需要后继节点
-                node_queue.print_data()
+                Graph.nodes[successor]['prev'] = current_node
+                
     min_distance = Graph.nodes[end]['min_dis']
+    path = []
+    prev_node = end
+    while prev_node:
+        path.insert(0, prev_node)
+        prev_node = Graph.nodes[prev_node]['prev']
 
-    return min_distance
+    return min_distance, path
 #%%
 if __name__ == '__main__':
     Nodes = ['s', 'a', 'b', 'c', 't']
@@ -54,9 +63,10 @@ if __name__ == '__main__':
     Graph = nx.Graph()
 
     for node in Nodes:
-        Graph.add_node(node, min_dis=0)
+        Graph.add_node(node, min_dis=0, prev=None)
 
     for key in arcs.keys():
         Graph.add_edge(key[0], key[1], length = arcs[key])
 
-    Dijkstra(Graph, 's', 't')
+    print(Dijkstra(Graph, 's', 't'))
+# %%
