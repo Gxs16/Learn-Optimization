f34f7522e2c87cc0ab5dba49206cb8cb9782eb59
X.Guo <847486005@qq.com>
2021-02-17 21:11:50 +0800

    Capacity Scaling uncomplete

---
+++ Graph Theory/Network Flow/Capacity Scaling.py
@@ -0,0 +1,86 @@
+'''
+@Author: Xinsheng Guo
+@Time: 2021年2月10日20:35:22
+@File: Edmonds Karp Algorithm.py
+'''
+
+import networkx as nx
+
+def find_augment_path(graph, delta):
+    '''
+    BFS
+    param:\n
+    graph:\n
+    return:\n
+    prev_dict\n
+    '''
+    queue = ['s']
+    visited = []
+    prev_dict = {}
+    while queue:
+        start = queue.pop(0)
+        for node in graph[start]:
+            if not node in visited and graph[start][node]['capacity']-graph[start][node]['cur_flow'] >= delta:
+                prev_dict[node] = start
+                visited.append(node)
+                if node == 't':
+                    return prev_dict
+                queue.append(node)
+    else:
+        return {}
+
+def get_max_flow(graph):
+    '''
+    param:\n
+    graph:\n
+    return:\n
+    graph:\n
+    '''
+    for edge in graph.edges():
+        
+    prev_dict = find_augment_path(graph, delta)
+    bottleneck_list = []
+    while prev_dict:
+        augment_value = []
+        node = 't'
+        while node != 's':
+            augment_value.append(graph[prev_dict[node]][node]['capacity']-graph[prev_dict[node]][node]['cur_flow'])
+            node = prev_dict[node]
+        bottleneck = min(augment_value)
+        bottleneck_list.append(bottleneck)
+        node = 't'
+        while node != 's':
+            graph[prev_dict[node]][node]['cur_flow'] += bottleneck
+            graph[node][prev_dict[node]]['cur_flow'] -= bottleneck
+            node = prev_dict[node]
+        prev_dict = find_augment_path(graph, delta)
+    return graph, bottleneck_list
+
+if __name__ == '__main__':
+    graph = nx.DiGraph()
+
+    edges = [('s', '0', 10), ('s', '1', 5), ('s', '2', 10),
+            ('0', 's', 0), ('1', 's', 0), ('2', 's', 0),
+            ('0', '3', 10),
+            ('3', '0', 0),
+            ('1', '2', 10),
+            ('2', '1', 0),
+            ('2', '5', 15),
+            ('5', '2', 0),
+            ('3', '1', 20), ('3', '6', 15),
+            ('1', '3', 0), ('6', '3', 0),
+            ('5', '4', 4), ('5', '8', 10),
+            ('4', '5', 0), ('8', '5', 0),
+            ('4', '1', 15), ('4', '3', 3),
+            ('1', '4', 0), ('3', '4', 0),
+            ('8', 't', 10),
+            ('t', '8', 0),
+            ('6', '7', 10), ('6', 't', 15),
+            ('7', '6', 0), ('t', '6', 0),
+            ('7', '4', 10), ('7', '5', 7),
+            ('4', '7', 0), ('5', '7', 0),
+            ]
+
+    graph.add_weighted_edges_from(edges, weight='capacity', cur_flow=0)
+    graph, max_flow = get_max_flow(graph)
+    print(max_flow)
--- Graph Theory/Network Flow/README.md
+++ Graph Theory/Network Flow/README.md
@@ -1,15 +1,21 @@
 # Network Flow
 
 ![Example](https://github.com/Gxs16/Learn-Optimization/blob/master/Graph%20Theory/Network%20Flow/maxflow.png)
+
 ## Ford-Fulkerson Method
 
 * Reference: <https://www.bilibili.com/video/BV11E411o7Fb?p=25>
 * Language: Python
 * Description: Depth First Search
 
-
 ## Edmonds Karp Algorithm
 
 * Reference: <https://www.bilibili.com/video/BV11E411o7Fb?p=30>
 * Language: Python
-* Description: Breath First Search
+* Description: Breath First Search.
+
+## Capacity Scaling (Uncomplete)
+
+* Reference: <https://www.bilibili.com/video/BV11E411o7Fb?p=30>
+* Language: Python
+* Description: The bottleneck value must be larger than a threshold we set.
