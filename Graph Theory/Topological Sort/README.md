6033fef8d1a5e9a1f8359e1a92d0fb1e109d955a
X.Guo <847486005@qq.com>
2021-03-24 23:03:52 +0800

    Topological Sort

---
+++ Graph Theory/Topological Sort/README.md
@@ -0,0 +1,7 @@
+# Topological Sort
+
+![Example](https://github.com/Gxs16/Learn-Optimization/blob/master/Graph%20Theory/Topological%20Sort/topological_sort.png)
+
+* Reference: <https://www.bilibili.com/video/BV11E411o7Fb?p=6>
+* Language: Python
+* Description: Depth First Search
---
+++ Graph Theory/Topological Sort/Topological Sort.py
@@ -0,0 +1,33 @@
+'''
+@Author: Xinsheng Guo
+@Time: 2021年3月21日22:24:25
+@File: Topological Sort.py
+'''
+def depth_first_search(graph, node, visit_set, sort_result):
+    if node in graph:
+        for i in graph[node]:
+            if not i in visit_set:
+                visit_set.append(i)
+                depth_first_search(graph, i, visit_set, sort_result)
+    sort_result.append(node)
+
+def topological_sort(graph, start_node):
+    visited = []
+    sort_result = []
+    depth_first_search(graph, start_node, visited, sort_result)
+    return sort_result[::-1]
+
+if __name__ == '__main__':
+    _graph = {'a': ['d'],
+             'b': ['d'],
+             'c': ['a', 'b'],
+             'd': ['h', 'g'],
+             'e': ['a', 'd', 'f'],
+             'f': ['k', 'j'],
+             'g': ['i'],
+             'h': ['j', 'i'],
+             'i': ['l'],
+             'j': ['l', 'm'],
+             'k': ['j']}
+
+    print(topological_sort(_graph, 'e'))
